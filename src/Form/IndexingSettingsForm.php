34039445abf4f672e27ff8e7288ca26dc8a551f8
nJim <jamesvomero@gmail.com>
2024-06-14 16:49:52 -0400

    Create indexing form

--- src/Form/IndexingSettingsForm.php
+++ src/Form/IndexingSettingsForm.php
@@ -5,10 +5,11 @@ namespace Drupal\dropai\Form;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Implements an entity Inspect form.
+ * Implements an indexing settings form for DropAI.
  */
 class IndexingSettingsForm extends ConfigFormBase {
 
@@ -20,13 +21,26 @@ class IndexingSettingsForm extends ConfigFormBase {
   protected $entityTypeManager;
 
   /**
-   * Constructs a new DropaiConfigForm.
+   * The entity type bundle info service.
    *
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager service.
+   * @var \Drupal\Core\Entity\EntityTypeBundleInfoInterface
    */
-  public function __construct(EntityTypeManagerInterface $entityTypeManager) {
+  protected $entityTypeBundleInfo;
+
+  /**
+   * Constructs a new IndexingSettingsForm.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The entity type manager service.
+   * @param \Drupal\Core\Entity\EntityTypeBundleInfoInterface $entityTypeBundleInfo
+   *   The entity type bundle info service.
+   */
+  public function __construct(
+    EntityTypeManagerInterface $entityTypeManager,
+    EntityTypeBundleInfoInterface $entityTypeBundleInfo
+  ) {
     $this->entityTypeManager = $entityTypeManager;
+    $this->entityTypeBundleInfo = $entityTypeBundleInfo;
   }
 
   /**
@@ -34,7 +48,8 @@ class IndexingSettingsForm extends ConfigFormBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('entity_type.manager')
+      $container->get('entity_type.manager'),
+      $container->get('entity_type.bundle.info')
     );
   }
 
@@ -59,46 +74,41 @@ class IndexingSettingsForm extends ConfigFormBase {
     $form['#tree'] = TRUE;
     $config = $this->config('dropai.indexing_settings');
 
-    // Get all content entity types.
-    $content_entity_types = [];
-    foreach ($this->entityTypeManager->getDefinitions() as $entity_type_id => $entity_type) {
-      if ($entity_type->entityClassImplements('Drupal\Core\Entity\ContentEntityInterface')) {
-        $content_entity_types[$entity_type_id] = $entity_type->getLabel();
-      }
-    }
-
-    $form['content_entities'] = [
+    // Form to select the content entities included in the vector index.
+    $content_entity_types = $this->getContentEntityTypes();
+    $form['entities'] = [
       '#type' => 'checkboxes',
       '#title' => $this->t('Content Entities'),
       '#description' => $this->t('Select the content entity types to be processed by DropAI.'),
       '#options' => $content_entity_types,
-      '#default_value' => array_keys($config->get('content_entities')) ?: [],
+      '#default_value' => array_keys($config->get('entities') ?? []),
       '#ajax' => [
         'callback' => '::updateBundleSettings',
         'wrapper' => 'bundle-settings-wrapper',
       ],
     ];
-
     $form['bundle_settings'] = [
       '#type' => 'container',
       '#attributes' => ['id' => 'bundle-settings-wrapper'],
     ];
 
-    $selected_entities = array_filter($form_state->getValue('content_entities', array_keys($config->get('content_entities')) ?: []));
-    foreach ($selected_entities as $entity_type_id) {
+    // Form to select the bundles that should be included in the vector index.
+    $selected = array_filter($form_state->getValue('entities', array_keys($config->get('entities') ?? [])));
+    foreach ($selected as $entity_type_id) {
       // Skip entities that do not have bundles.
       if (!$this->hasBundles($entity_type_id)) {
         continue;
       }
-      $bundles = \Drupal::service('entity_type.bundle.info')->getBundleInfo($entity_type_id);
 
+      $bundles = $this->entityTypeBundleInfo->getBundleInfo($entity_type_id);
       $bundle_options = [];
       foreach ($bundles as $bundle_id => $bundle) {
         $bundle_options[$bundle_id] = $bundle['label'];
       }
 
       $form['bundle_settings'][$entity_type_id] = [
-        '#type' => 'fieldset',
+        '#type' => 'details',
+        '#closed' => TRUE,
         '#title' => $content_entity_types[$entity_type_id] . ' ' . $this->t('Bundle Settings'),
       ];
 
@@ -109,16 +119,15 @@ class IndexingSettingsForm extends ConfigFormBase {
           'include' => $this->t('Only those selected'),
           'exclude' => $this->t('All except those selected'),
         ],
-        '#default_value' => $config->get('content_entities')[$entity_type_id]['index_type'] ?? 'include',
+        '#default_value' => $config->get('entities')[$entity_type_id]['index_type'] ?? 'exclude',
       ];
 
       $form['bundle_settings'][$entity_type_id]['bundles'] = [
         '#type' => 'checkboxes',
         '#title' => $this->t('Bundles'),
         '#options' => $bundle_options,
-        '#default_value' => $config->get('content_entities')[$entity_type_id]['bundles'] ?? [],
+        '#default_value' => $config->get('entities')[$entity_type_id]['bundles'] ?? [],
       ];
-
     }
 
     return parent::buildForm($form, $form_state);
@@ -136,38 +145,48 @@ class IndexingSettingsForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $config = $this->config('dropai.indexing_settings');
-    $content_entities = array_filter($form_state->getValue('content_entities'));
+    $entities = array_filter($form_state->getValue('entities'));
+
     $entities_settings = [];
-    foreach ($content_entities as $entity_type_id) {
+    foreach ($entities as $entity_type_id) {
       $entities_settings[$entity_type_id] = [
-        'index_type' => $form_state->getValue(['bundle_settings', $entity_type_id, 'index_type'], 'any'),
-        'bundles' => $form_state->getValue(['bundle_settings', $entity_type_id, 'bundles']),
+        'index_type' => $form_state->getValue(['bundle_settings', $entity_type_id, 'index_type'], 'exclude'),
+        'bundles' => array_filter($form_state->getValue(['bundle_settings', $entity_type_id, 'bundles']) ?? []),
       ];
     }
 
-    $config->set('content_entities', $entities_settings)->save();
+    $config->set('entities', $entities_settings)->save();
 
     parent::submitForm($form, $form_state);
   }
 
   /**
-   * Returns the definition of this datasource's entity type.
+   * Retrieves the list of content entity types.
    *
-   * @return \Drupal\Core\Entity\EntityTypeInterface
-   *   The entity type definition.
+   * @return array
+   *   An array of content entity type labels keyed by entity type ID.
    */
-  protected function getEntityTypeDefinition($entity_type_id) {
-    return $this->entityTypeManager->getDefinition($entity_type_id);
+  protected function getContentEntityTypes() {
+    $content_entity_types = [];
+    foreach ($this->entityTypeManager->getDefinitions() as $entity_type_id => $entity_type) {
+      if ($entity_type->entityClassImplements('Drupal\Core\Entity\ContentEntityInterface')) {
+        $content_entity_types[$entity_type_id] = $entity_type->getLabel();
+      }
+    }
+    return $content_entity_types;
   }
 
   /**
    * Determines whether the entity type supports bundles.
    *
+   * @param string $entity_type_id
+   *   The entity type ID.
+   *
    * @return bool
    *   TRUE if the entity type supports bundles, FALSE otherwise.
    */
   protected function hasBundles($entity_type_id) {
-    return $this->getEntityTypeDefinition($entity_type_id)->hasKey('bundle');
+    return $this->entityTypeManager->getDefinition($entity_type_id)->hasKey('bundle');
   }
 
 }
